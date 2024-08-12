205975bc9eabb78921bd56e60b1104bf3129bef2
Prashant Srivastava <theprashantsri@gmail.com>
2024-08-12 10:57:22 +0530

    add

---
+++ file1.js
@@ -0,0 +1,22 @@
+let a = 10;
+document.write(a,"<br/>")
+let b = 5;
+document.write(b, "<br/>")
+let sum = a+b;
+document.write(sum, "<br/>")
+let difference = a-b;
+document.write(difference, "<br/>")
+let product = a*b;
+document.write(product, "<br/>")
+let quotient = a/b;
+document.write(quotient, "<br/>")
+let remainder = a%b;
+document.write(remainder, "<br/>")
+let isEqual = (a==b);
+document.write(isEqual, "<br/>")
+let isNotEqual = (a !=b);
+document.write(isNotEqual, "<br/>")
+let isGerater = (a>b);
+document.write(isGerater, "<br/>")
+let logicalAnd = (a>b && b<10);
+document.write(logicalAnd, "<br/>")
