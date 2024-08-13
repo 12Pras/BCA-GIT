495f83464875c3a3d91d2dd3f628d3b3cc644f00
Prashant Srivastava <theprashantsri@gmail.com>
2024-08-13 15:38:03 +0530

    second

--- file1.js
+++ file1.js
@@ -1,22 +1,44 @@
-let a = 10;
-document.write(a,"<br/>")
-let b = 5;
-document.write(b, "<br/>")
-let sum = a+b;
-document.write(sum, "<br/>")
-let difference = a-b;
-document.write(difference, "<br/>")
-let product = a*b;
-document.write(product, "<br/>")
-let quotient = a/b;
-document.write(quotient, "<br/>")
-let remainder = a%b;
-document.write(remainder, "<br/>")
-let isEqual = (a==b);
-document.write(isEqual, "<br/>")
-let isNotEqual = (a !=b);
-document.write(isNotEqual, "<br/>")
-let isGerater = (a>b);
-document.write(isGerater, "<br/>")
-let logicalAnd = (a>b && b<10);
-document.write(logicalAnd, "<br/>")
+// // //let a = 10;
+// // //document.write(a,"<br/>")
+// // //let b = 5;
+// // //document.write(b, "<br/>")
+// // //let sum = a+b;
+// // //document.write(sum, "<br/>")
+// // //let difference = a-b;
+// // //document.write(difference, "<br/>")
+// // //let product = a*b;
+// // //document.write(product, "<br/>")
+// // //let quotient = a/b;
+// // //document.write(quotient, "<br/>")
+// // //let remainder = a%b;
+// // //document.write(remainder, "<br/>")
+// // //let isEqual = (a==b);
+// // //document.write(isEqual, "<br/>")
+// // //let isNotEqual = (a !=b);
+// // //document.write(isNotEqual, "<br/>")
+// // //let isGerater = (a>b);
+// // //document.write(isGerater, "<br/>")
+// // //let logicalAnd = (a>b && b<10);
+// // //document.write(logicalAnd, "<br/>")
+
+
+// // let fname = "john";
+// // document.write("hello",fname,"<br/>") 
+// // let age = "30"
+// // document.write("Age",age,"<br/>")
+// // let isStudent = true;
+// // document.write(`Student - ${isStudent? "Yes":"No"}<br/>`)
+// // let emptyValue = null;
+// // document.write(`About us ${emptyValue || "Write Something"}<br/>`)
+// // let uninitialized;
+// // document.write(!uninitialized && "Fill detail completely")
+
+// let number = prompt("Enter number");
+
+// if(number>0){
+//     console.log("the number is positive.");
+// }else if(number <0){
+//     console.log("the number is negative.");
+// }else{
+//     console.log("The number is zero.")
+// }
